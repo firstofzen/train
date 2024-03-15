7b147274e53faefec83b12ac48ca70f0fecb6f5c
firstofzen <firstofzen@gmail.com>
2024-03-15 17:44:47 +0700

    complete training

---
+++ src/utils/cn.ts
@@ -0,0 +1,5 @@
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#0e100f",
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
